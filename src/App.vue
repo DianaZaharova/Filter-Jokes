98a32fb72e9d556b587a77451e4c2a8e32c5d52a
Diana Zaharova <Dysua1999@mail.ru>
2021-06-30 17:10:42 +0300

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Search/>
+    <List/>
+  </div>
+</template>
+
+<script>
+import List from "./components/List.vue"
+import Search from "./components/List.vue"
+
+export default {
+  name: 'App',
+  components: {
+    List, Search
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
